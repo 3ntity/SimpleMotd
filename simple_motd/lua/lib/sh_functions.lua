70ea5e145177ba7533e7f65d6d0383bc7f671759
3ntity <anonymous77012@gmail.com>
2014-10-08 22:41:49 +0100

    Minor Changes

--- simple_motd/lua/lib/sh_functions.lua
+++ simple_motd/lua/lib/sh_functions.lua
@@ -14,29 +14,37 @@ end
 
 --[[ Cuz of the enter and exit i decide to give the nav button an extra function ]]--
 function AddNavButton( id , x , y , w , h , text, page)
+
 	local NavButtonColor = Color( 46,54,65, 255 )
+	local NavTextColor = Color(255,255,255,200)
 	local NavButtonOne = vgui.Create( "DButton", MotdNav )
+
 	NavButtonOne:SetPos( x, y )
 	NavButtonOne:SetText( "" )
 	NavButtonOne:SetSize( w, h )
-	NavButtonOne.Paint =  function()
+	NavButtonOne.Paint = function()
 		draw.RoundedBox( 0, 0, 0, NavButtonOne:GetWide(), NavButtonOne:GetTall(), NavButtonColor )
-		draw.SimpleText(text,"SMOTD_Body",NavButtonOne:GetWide()/2,NavButtonOne:GetTall()/2,Color(255,255,255,200),TEXT_ALIGN_CENTER)
+		draw.SimpleText(text,"SMOTD_Body",NavButtonOne:GetWide()/2,NavButtonOne:GetTall()/2,NavTextColor,TEXT_ALIGN_CENTER)
 	end
+
 	NavButtonOne.OnCursorEntered = function()
 		surface.PlaySound("garrysmod/ui_return.wav")
 		NavButtonColor = Color( 255, 255, 255, 255 )
+		NavTextColor = Color(46,54,65, 255)
+
 	end
 
 	NavButtonOne.OnCursorExited = function()
-		NavButtonColor = Color( 46,54,65, 255 )
+	NavButtonColor = Color( 46,54,65, 255 )
+	NavTextColor = Color(255,255,255,200)
 	end
-
 
 	NavButtonOne.DoClick = function()
 		print("clicked Button "..id)
 	end
+
 end
+
 
 function AddPage(id) -- Need somesort of UID for the Page VGUI to toggle them.
 	local Title = vgui.Create( "DLabel", MotdMain )
