ad2d4880a00fd2c0de24246cb560ab5d3e0fbf13
3ntity <anonymous77012@gmail.com>
2014-10-08 22:40:32 +0100

    Minor Changes

--- simple_motd/lua/autorun/client/cl_motd.lua
+++ simple_motd/lua/autorun/client/cl_motd.lua
@@ -1,5 +1,6 @@
---[[ SkateMOTD ]]--
+--[[ SimpleMotd ]]--
 --[[ Includes ]]--
+--[[ Created by Tomelyr and _3ntity ]]
 include('sh_config.lua')
 include('lib/sh_colors.lua')
 include('lib/sh_functions.lua')
@@ -28,7 +29,7 @@ function BuildMOTD()
 	MotdNav.Paint =  function()
 		draw.RoundedBox( 0, 0, 0, MotdNav:GetWide(), MotdNav:GetTall(), Color( 46,54,65, 255 ) )
 	end
-	
+
 	NavExtra = vgui.Create( "DPanel", MotdMain) -- Just the small bar to the right
 	NavExtra:SetPos( 100, 0 ) 
 	NavExtra:SetSize( 10	, 400 ) 
@@ -57,10 +58,30 @@ function BuildMOTD()
 		draw.RoundedBox( 0, 0, 0, body:GetWide(), body:GetTall(), Color( 218,228,235, 255 ) )
 	end
 
+	--[[THIS IS WHERE YOU WANT TO ADD YOUR OWN THINGS]]--
+	-- I advise you use numbers for the id's. 
+
+	/*--[[Key:]]--
+	
+	id = A nuber ID. This can be anything. 1 or 5 or 1000. Your Choice.
+	x = the x position
+	y = the y position
+	w = the width of the element. This is your choice. 100 or 50 or 60.... Anything
+	h = the height of the element. Same rule as width.
+	text = the text of the element. Ie. "Blah" make sure your text is incased in quotation marks!
+	page = To be defined
+	url = To be defined.
+
+
+
+	--*/--
+
+	-- AddNavButton(id, x, y, w, h, text, page)
+	-- 
 	AddNavButton(1,0,0,100,100, "test")
 	AddNavButton(2,0,100,100,100, "test2")
 	AddNavButton(3,0,200,100,100, "test3")
-	AddNavButton(4,0,300,100,100, "test4")
+	AddNavButton(4,0,300,100,100, "test5")
 	AddPage(1)
 	AddPage(2)
 	AddPage(3)
